33ff56952c8b3d95f9b3e22f25eb921d25f2a07e
Felilepe <felipesepulveda536@gmail.com>
2026-03-24 15:13:33 -0300

    feat:implementa função que trata do abrimento de arquivos

--- hte.c
+++ hte.c
@@ -30,6 +30,46 @@ typedef struct
 }hte_directory;
 
 
+Hash hash_openFile(const char* filename)
+{
+    hte_directory* dir = malloc(sizeof(hte_directory));
+    if(dir == NULL) return NULL;
 
+    FILE* f = fopen(filename, "rb+");
 
+    if(f == NULL){
+        f = fopen(filename,"wb+");
+        if(f == NULL){
+            free(dir);
+            printf("ERRO ao criar ficheiro no disco");
 
+            return NULL;
+        }
+        printf("Nenhum fichario com o nome '%s' encontrado. Criando novo fichario...\n", f);
+
+        dir -> disk_file = f;
+        dir -> global_depth = 0;
+        dir -> directory_size = 1;
+        dir -> bucket_offsets = malloc(dir -> directory_size * sizeof(long));
+
+        bucket initial_bucket;
+        initial_bucket.local_depth = 0;
+        initial_bucket.record_count = 0;
+
+        for (int i = 0; i < RECORDS_PER_BUCKET; i++) {
+            initial_bucket.records[i].is_occupied = false;
+        }
+
+        dir->bucket_offsets[0] = 0;
+
+        fseek(f, 0, SEEK_SET);
+        fwrite(&initial_bucket, sizeof(bucket), 1, f);
+
+        printf("Novo fichario Hash '%s' criado\n", f);
+    }
+
+    else{
+        dir->disk_file = f;
+        printf("Ficheiro Hash '%s' aberto com sucesso\n", filename);
+    }
+}
